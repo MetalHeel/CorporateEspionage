ce76da1257b41b0fb9abddf413a184da9be02d07
MetalHeel <MetalHeel@aol.com>
2017-02-24 21:36:27 -0600

    Add all the Unity stuff.

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraBehavior : MonoBehaviour
+{
+    public GameObject player;
+
+    private float speed = 0.1f;
+
+    void Start()
+    {
+    }
+
+    // Todo: Have a threshold position maybe.
+    void Update()
+    {
+        Camera camera = GetComponent<Camera>();
+
+        Vector3 position = transform.position;
+
+        float height = 2f * camera.orthographicSize;
+        float width = height * camera.aspect;
+
+        float heightQ = height / 4.0f;
+        float widthQ = width / 4.0f;
+
+        if (player.transform.position.x < transform.position.x - (2 * widthQ))
+            position.x -= speed;
+
+        if (player.transform.position.x > transform.position.x + (2 * widthQ))
+            position.x += speed;
+
+        if (player.transform.position.y > transform.position.y + (2 * heightQ))
+            position.y += speed;
+
+        if (player.transform.position.y < transform.position.y - (2 * heightQ))
+            position.y -= speed;
+    }
+}
---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerBehavior : MonoBehaviour
+{
+    private float speed = 0.1f;
+
+	void Start ()
+    {	
+	}
+	
+	void Update ()
+    {
+        Vector3 position = transform.position;
+
+        if (Input.GetKey(KeyCode.A))
+            position.x -= speed;
+
+        if (Input.GetKey(KeyCode.D))
+            position.x += speed;
+
+        if (Input.GetKey(KeyCode.W))
+            position.z += speed;
+
+        if (Input.GetKey(KeyCode.S))
+            position.z -= speed;
+
+        transform.position = position;
+	}
+}
